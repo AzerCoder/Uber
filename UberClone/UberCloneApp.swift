271cc079572503270a6c1142ad718a67cbdf4cf2
A'zamjon Abdumuxtorov <azamjonabdumuxtorov1220@gmail.com>
2024-09-24 12:04:05 +0500

    Initial Commit

---
+++ UberClone/UberCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  UberCloneApp.swift
+//  UberClone
+//
+//  Created by A'zamjon Abdumuxtorov on 24/09/24.
+//
+
+import SwiftUI
+
+@main
+struct UberCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
